552d863e532c4a576f255d10f47706fc3b6b4e68
sastolarski <sastolarski@gmail.com>
2018-04-28 17:34:54 -0500

    newQuestion function starting to work

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -17,48 +17,51 @@ var ding = new Audio();
 var buzz = new Audio();
 var seconds;
 
+
 var triviaQuestions = [{
-        question1 : "Cleopatra lived closer to today than to when the pyramids were built",
+        question : "Cleopatra lived closer to today than when the pyramids were built",
         answers : ["true", "false"],
         answer : 0
     },{
-        question2 : "Gwen Stefani is younger than Ted Cruz",
+        question : "Gwen Stefani is younger than Ted Cruz",
         answers : ["true", "false"],
         answer : 1
     },{
-        question3 : "In the span of the release of the last 3 Star Wars movies, the Cleaveland Browns have won 1 game",
+        question : "In the span of the release of the last 3 Star Wars movies, the Cleaveland Browns have won 1 game",
         answers : ["true", "false"],
         answer : 0
     },{
-        question4 : "Anne Frank and Martin Luther King Jr. were born in different years",
+        question : "Anne Frank and Martin Luther King Jr. were born in different years",
         answers : ["true", "false"],
         answer : 1
     },{
-        question5 : "A pineapple takes a year to grow to maturity",
+        question : "A pineapple takes a year to grow to maturity",
         answers : ["true", "false"],
         answer : 1
     },{
-        question6 : "There are more chickens in America than people on the planet",
+        question : "There are more chickens in America than people on the planet",
         answers : ["true", "false"],
         answer : 0
     },{
-        question7 : "Sharks predate trees",
+        question : "Sharks predate trees",
         answers : ["true", "false"],
         answer : 0
     },{
-        question8 : "Sir Christoper Lee witnessed the last public execution in France",
+        question : "Sir Christoper Lee witnessed the last public execution in France",
         answers : ["true", "false"],
         answer : 0
     },{
-        question9 : "The human body consists of (roughly) a 2-to-1 ratio fo bacterial to human cells",
+        question : "The human body consists of (roughly) a 2-to-1 ratio fo bacterial to human cells",
         answers : ["true", "false"],
         answer : 1
     },{
-        question10 : "The entire continent of South America is east of Florida",
+        question : "The entire continent of South America is east of Florida",
         answers : ["true", "false"],
         answer : 0
     
 }];
+
+var transitionMessages = ["Correct!", "Wrong!", "Let's see how you did!"]
 
 
 $(document).ready(function(){
@@ -69,14 +72,33 @@ $(document).ready(function(){
     });
 
     function gameReset(){
+        $("#questionNum").empty();
         $("#question").empty();
         $("#answerChoices").empty();
         correctCounter = 0;
         wrongCounter = 0;
         unanswered = 0;
+        currentQuestion = 0;
+        newQuestion();
         //also display new question function
         //new question function will include timer 
     };
+
+    function newQuestion(){
+        $("#questionNum").empty();
+        $("#question").empty();
+        $("#answerChoices").empty();
+        $("#questionNum").html("Question #"+(currentQuestion+1)+"/"+triviaQuestions.length);
+        $("#question").html("<h2>" + triviaQuestions[currentQuestion].question + "</h2>");
+        for (var i = 0; i < 2; i++) {
+            answerChoices = $("<div>");
+            answerChoices.text(triviaQuestions[currentQuestion].answers[i]);
+            $("#answerChoices").append(answerChoices);
+        }
+
+    }
+
+           // currentQuestion++; put this in the click handler that goes with answer selection and time running out
 
 
     
