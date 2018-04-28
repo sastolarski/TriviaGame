bf11280f41995effea99631f50b2905904a06779
sastolarski <sastolarski@gmail.com>
2018-04-28 15:51:43 -0500

    started

---
+++ assets/javascript/app.js
@@ -0,0 +1,6 @@
+//global vars- correctCounter, wrongCounter, unanswered
+//press start to begin button
+//on start 1st question appears along with 1st question true or falses
+//timer starts with 5 seconds on the clock
+//if the user chooses the correct answer, immediatly move to the next question, add 1 to correctCounter
+//if the users answers a question incorrectly, same thing but add 1 to wrongCounter
